3b811ad79adcb90fdd12eda7a0af2225c8acd36a
Tannya Granados <tannyagranados@Tannyas-MacBook-Pro.local>
2023-07-31 20:14:45 -0600

    Tabla de usuarios

--- php/tablas.php
+++ php/tablas.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ingresar Usuario</title>
     <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/tablas.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
@@ -18,7 +19,16 @@
     ?>
 
     <main>
-        
+        <div class="contenedorAdminTablas">
+            <h1>Administración de Tablas</h1>
+        </div>
+        <div class="listarTablas">
+            <ul>
+                <li><a href="tablaUsuarios.php">Usuarios</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
     </main>
 
 
